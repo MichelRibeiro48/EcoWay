2f12cbdca9f43856400e5934cbdd04b57db9273e
MichelRibeiro48 <michel-amoral@hotmail.com>
2023-04-22 14:02:46 -0300

    Adding RadioButton and upload images

--- src/pages/ReportPage/index.tsx
+++ src/pages/ReportPage/index.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React from "react";
+import React, { useState } from "react";
 import {
   Image,
   Text,
@@ -13,19 +13,26 @@ import {
   useFonts,
   Roboto_100Thin_Italic,
   Roboto_500Medium,
+  Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import IconC from "@expo/vector-icons/FontAwesome";
 import IconR from "@expo/vector-icons/AntDesign";
 import IconP from "@expo/vector-icons/Entypo";
 import IconF5 from "@expo/vector-icons/FontAwesome5";
+import IconF from "@expo/vector-icons/FontAwesome";
 import IconFe from "@expo/vector-icons/Feather";
 import styles from "./styles";
 import CardLocation from "../../components/CardLocation";
+import { RadioButton } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
 
 export default function ReportPage({ navigation, route }) {
+  const [checked, setChecked] = useState("");
+  const [imageCamera, setImageCamera] = useState(null);
   const [fontsLoaded] = useFonts({
     Roboto_100Thin_Italic,
     Roboto_500Medium,
+    Roboto_700Bold,
   });
 
   if (!fontsLoaded) {
@@ -33,6 +40,19 @@ export default function ReportPage({ navigation, route }) {
   }
   const { title, image, numberReported, Address, RecycleSupport } =
     route.params;
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImageCamera(result.assets[0].uri);
+    }
+  };
   return (
     <View className="flex-1 justify-center">
       <Image
@@ -48,7 +68,7 @@ export default function ReportPage({ navigation, route }) {
           title={title}
         />
         <View
-          className="w-full h-20 bg-White rounded-xl flex-row px-2 py-1 mt-6"
+          className="w-full h-28 bg-White rounded-xl flex-row px-2 py-1 my-6"
           style={[
             Platform.OS === "android" ? styles.AndroidShadow : styles.IosShadow,
           ]}
@@ -59,26 +79,68 @@ export default function ReportPage({ navigation, route }) {
             multiline
           />
         </View>
+        <View className="flex-row items-center">
+          <RadioButton
+            color="#576032"
+            uncheckedColor="#576032"
+            value="empty"
+            status={checked === "empty" ? "checked" : "unchecked"}
+            onPress={() => setChecked("empty")}
+          />
+          <Text>Vazio</Text>
+        </View>
+        <View className="flex-row items-center">
+          <RadioButton
+            color="#576032"
+            uncheckedColor="#576032"
+            value="empty"
+            status={checked === "parcial" ? "checked" : "unchecked"}
+            onPress={() => setChecked("parcial")}
+          />
+          <Text>Parcialmente cheio</Text>
+        </View>
+        <View className="flex-row items-center">
+          <RadioButton
+            color="#576032"
+            uncheckedColor="#576032"
+            value="empty"
+            status={checked === "full" ? "checked" : "unchecked"}
+            onPress={() => setChecked("full")}
+          />
+          <Text>Cheio</Text>
+        </View>
         <TouchableOpacity
           className="mt-10 w-60 h-16 bg-White self-center items-center justify-center rounded-lg flex-row"
           style={[
             Platform.OS === "android" ? styles.AndroidShadow : styles.IosShadow,
           ]}
-          onPress={() => console.log("hello world")}
+          onPress={pickImage}
         >
           <IconR name="addfile" size={24} color={"black"} />
           <Text className="text-Black ml-1">Anexar uma foto do local</Text>
         </TouchableOpacity>
+        {imageCamera !== null && (
+          <Text className="self-center mt-4">Foto anexada com sucesso</Text>
+        )}
         <View className="flex-row justify-between mt-auto">
-          <TouchableOpacity className="mt-4 w-28 h-12 bg-Green self-center items-center justify-center rounded-lg flex-row">
+          <TouchableOpacity
+            className="w-28 h-12 bg-Green self-center items-center justify-center rounded-lg flex-row"
+            onPress={() => navigation.navigate("MapPage")}
+          >
             <IconP name="location-pin" size={24} color={"white"} />
             <Text className="text-White ml-1">Rotas</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            className="mt-4 w-28 h-12 bg-Green self-center items-center justify-center rounded-lg"
-            onPress={() => navigation.goBack()}
+            className="w-28 h-12 bg-Green self-center items-center justify-center rounded-lg flex-row"
+            onPress={() => console.log("Foto enviada com sucesso!")}
           >
-            <IconC name="close" size={32} color={"white"} />
+            <IconF name="send" size={16} color={"white"} />
+            <Text
+              style={{ fontFamily: "Roboto_700Bold" }}
+              className="text-White ml-2 text-base"
+            >
+              Enviar
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
