af8fdb7c7fe6d31966c876956fe07e4dbf6d8ff0
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-05-09 19:53:24 -0300

    Fix: Posts page

--- src/pages/TipsRecyclePage/index.tsx
+++ src/pages/TipsRecyclePage/index.tsx
@@ -21,13 +21,15 @@ export default function TipsRecyclePage({ navigation }) {
 
   return (
     <View className="flex-1 items-center justify-center pt-16 bg-White rounded">
-      <View className="flex-row self-start pl-3">
+      <View className="flex-row self-start px-5">
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <IconI name="chevron-back-outline" size={32} color={'#576032'} />
         </TouchableOpacity>
         <Text
-          className="text-3xl text-Green ml-6 mb-8"
+          className="w-full text-2xl text-Green ml-2 mb-8 max-w-[80%]"
           style={{ fontFamily: 'Roboto_500Medium' }}
+          ellipsizeMode="tail"
+          numberOfLines={1}
         >
           Dicas de Reciclagem
         </Text>
@@ -36,6 +38,7 @@ export default function TipsRecyclePage({ navigation }) {
         data={TipsList}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => <PostCard post={item} />}
+        className="px-5"
       />
     </View>
   )
